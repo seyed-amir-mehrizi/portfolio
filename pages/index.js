de2d42a29629a478e683c6213079733fbc4af5da
seyed-amir-mehrizi <60378911+seyed-amir-mehrizi@users.noreply.github.com>
2022-12-24 15:41:03 +0330

    make nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { BsFillMoonStarsFill } from 'react-icons/bs'
 export default function Home() {
   return (
     <>
@@ -8,7 +9,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className='bg-white px-10'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-burtons'>
+              developed By Amir
+            </h1>
+            <ul className='flex items-center'>
+              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl' /></li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500  bg-cyan-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
+            </ul>
+          </nav>
+
+          <div>
+            <h2>Seyed Amir Mehrizi</h2>
+            <h3>Developer and English Teacher.</h3>
+            <p>sdc;l,scl</p>
+          </div>
+        </section>
       </main>
     </>
   )
